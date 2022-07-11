a123f4f234d302d09f3e9e2f964948503c6ab9cc
Claudio Junior <cla.junior21@gmail.com>
2022-07-11 11:09:09 -0300

    init: subindo o projeto

---
+++ bagarote/src/main/java/br/dev/karyon/bagarote/BagaroteApplication.java
@@ -0,0 +1,13 @@
+package br.dev.karyon.bagarote;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagaroteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagaroteApplication.class, args);
+	}
+
+}
---
+++ bagarote/src/test/java/br/dev/karyon/bagarote/BagaroteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.karyon.bagarote;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagaroteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
